010c863e02bf49c27deb66733d1371820cd94461
Ethan-Rivas <ethan-rivas@hotmail.com>
2021-09-27 18:33:07 -0500

    first commit

---
+++ js/main.js
@@ -0,0 +1,27 @@
+// Mostrar formulario para crear comentario
+const button = $('#create-comment > button');
+
+button.on('click', function() {
+    $('#new-comment').show();
+    $(this).hide();
+});
+
+// Reiniciar formulario al presionar botón de cancelar
+$('#cancel-comment').on('click', function() {
+    $('#new-comment').hide();
+
+    const form = $('form');
+    form.trigger("reset");
+    form.parent().find('button#publish-comment').prop('disabled', false);
+
+    button.show();
+});
+
+// Mostrar únicamente controles de video al pasar el cursor en el video
+$('#video-player').hover(function toggleControls() {
+    if (this.hasAttribute("controls")) {
+        this.removeAttribute("controls")
+    } else {
+        this.setAttribute("controls", "controls")
+    }
+})
